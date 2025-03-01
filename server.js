ab6a13865e2584ac1a940d2620c83f8b1785b242
Denisraj10 <denisraj2122@gmail.com>
2025-03-01 22:15:58 +0530

    vercel

--- server.js
+++ server.js
@@ -114,3 +114,6 @@ app.get("/getForms", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+module.exports = app;
